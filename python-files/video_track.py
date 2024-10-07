ee9e18667329b9e6853227fca71c305d2456ffaf
Gabe H <gabeh@reed.edu>
2024-10-07 09:59:45 -0700

    updating

--- python-files/video_track.py
+++ python-files/video_track.py
@@ -4,6 +4,8 @@ import numpy as np
 import argparse
 import cv2 as cv
 import time
+import matplotlib.pyplot as plt
+
 
 def main(args):
     
@@ -12,7 +14,78 @@ def main(args):
     print(f"testing various streams")
     for stream in args.input:
         print(f"testing stream through video {stream}")
-        capVid(stream)
+        #capVid(stream)
+        blob_dect(stream)
+
+def blob_dect(in_stream):
+    # Try to get a video captured
+    fstream = in_stream
+    cap = cv.VideoCapture(fstream)
+    print("setup cap, starting loop")
+
+    if not cap.isOpened():
+        print("ERR")
+        exit()
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        print(ret)
+
+        frame = cv.flip(frame, 0)
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+
+
+        # Initializing parameter setting using cv.SimpleBlobDetector function
+        params = cv.SimpleBlobDetector_Params()
+        
+        # Filter by area (value for area here defines the pixel value)
+        params.filterByArea = True
+        params.minArea = 100
+        
+        # Filter by circularity
+        params.filterByCircularity = True
+        params.minCircularity = 0.75
+        
+        # Filter by convexity
+        params.filterByConvexity = True
+        params.minConvexity = 0.2
+            
+        # Filter by inertia ratio
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.01
+        
+        # Creating a blob detector using the defined parameters
+        detector = cv.SimpleBlobDetector_create(params)
+            
+        # Detecting the blobs in the image
+        keypoints = detector.detect(rgb)
+        
+        # Drawing the blobs that have been filtered with blue on the image
+        blank = np.zeros((1, 1))
+        blobs = cv.drawKeypoints(rgb, keypoints, blank, (0, 0, 0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        
+        cv.imshow("original", rgb)
+        cv.imshow("blobs", blobs)
+        
+        # Setting the grid size
+        plt.figure(figsize=(20,20))
+        
+        # Displaying the image
+        plt.subplot(121)
+        plt.title('Original')
+        plt.imshow(rgb, cmap='gray')
+        
+        plt.subplot(122)
+        plt.title('Blobs')
+        plt.imshow(blobs)
+        
+        plt.show()
+
 
 def capVid(in_stream):
     # Try to get a video captured
@@ -52,6 +125,7 @@ def capVid(in_stream):
 
         result2 = cv.bitwise_and(result, result, mask = mask)
         mask2 = np.array(mask)
+        result_blur = cv.GaussianBlur(result2, (7,7), 10)
 
         img_array = np.array(result2)
 
@@ -62,11 +136,13 @@ def capVid(in_stream):
 
         cv.circle(result2, maxLoc, 100, (255,234,132))
         cv.circle(mask2, maxLoc, 100, (255,234,132))
+        cv.circle(result_blur, maxLoc, 100, (255,234,132))
 
 
         cv.imshow('frame', frame)
         cv.imshow('mask', mask2)
         cv.imshow('result', result2)
+        cv.imshow('blur', result_blur)
         time.sleep(0.1)
         #gray = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
